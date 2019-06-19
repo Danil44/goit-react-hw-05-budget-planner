4334ce427e83eef2065159911b02e56bb97f3cb5
Daniil Golovenko (CODER) <d.golovenko@itcg.ua>
2019-06-19 18:09:54 +0300

    Updates

---
+++ precache-manifest.8ec6ddc289b3d6b5bb341331b62ec338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "fe0c65335766e4c441a0",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.4e897e26.chunk.js"
+  },
+  {
+    "revision": "7d9a67f70707485cb9ce",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.98b189ab.chunk.js"
+  },
+  {
+    "revision": "fe0c65335766e4c441a0",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.aa3e11b3.chunk.css"
+  },
+  {
+    "revision": "fc5b715bdfdda07878e9f4f580aadd06",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
